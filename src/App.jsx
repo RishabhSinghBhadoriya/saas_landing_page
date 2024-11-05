56dbe4f9a048a55525a700382d4a68cd2de520cf
RishabhSinghBhadoriya <rishabhsingh9691@gmail.com>
2024-11-05 22:10:27 +0530

    added Testimonials items component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Features } from './sections/Features';
 import { Header } from './sections/Header'
 import { MainPage } from './sections/MainPage';
 import { Pricing } from './sections/Pricing';
+import { Testimonials } from './sections/Testimonials';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Features/>
       <Pricing/>
       <Faq/>
+      <Testimonials/>
     </main>
   );
 };
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -1,4 +1,8 @@
+import { TestItems } from "../components/TestItems";
+import { testimonials } from "../content";
+
 export const Testimonials=()=>{
+    const halfLength=Math.floor(testimonials.length/2);
     return(
         <section className="relative z-2 py-24 md:py-28 lg:py-40">
             <div className="container block lg:flex">
@@ -8,7 +12,12 @@ export const Testimonials=()=>{
                     </h3>
                 </div>
                 <div className="testimonials_inner-after testimonials_inner-before relative -my-12 -mr-3 flex items-start max-lg:static max-md:block ">
-                    <div className="testimonials_group-after flex-50"></div>
+                    <div className="testimonials_group-after flex-50">
+                        {testimonials.slice(0,halfLength).map((testimonial)=><TestItems key={testimonial.id} item={testimonial} containerClassName="last:after:hidden last:after:max-md:block"/>)}
+                    </div>
+                    <div className="flex-50">
+                    {testimonials.slice(0,halfLength).map((testimonial)=><TestItems key={testimonial.id} item={testimonial} containerClassName="last:after:hidden after:right-auto"/>)}
+                    </div>
                 </div>
             </div>
         </section>
